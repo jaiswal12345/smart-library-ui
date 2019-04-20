2d361a9c464cb8522b74fd33f694e50e0d5d0e32
Piyush Jaiswal <piyush.jaiswal@ad.kaleyra.local>
2019-04-20 15:52:56 +0530

    new book demo

---
+++ src/app/new-book/new-book.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-book',
+  templateUrl:'./new-book.component.html',
+})
+export class NewBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
